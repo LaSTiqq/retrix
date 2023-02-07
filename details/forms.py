01c75b69e1a5de1d14671f66eff9c273c4f827f2
LaSTiqq <lavrencij13@gmail.com>
2023-02-07 21:01:29 +0200

    Updated placeholders for contact form, improved hex color codes for social media icons

--- details/forms.py
+++ details/forms.py
@@ -2,20 +2,20 @@ from django import forms
 # from captcha.fields import CaptchaField, CaptchaTextInput
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Vārds', widget=forms.TextInput(attrs={'class': 'form-control  mt-2 mx-auto', 'placeholder': 'Vārds/имя/name',
+    name = forms.CharField(label='Vārds', widget=forms.TextInput(attrs={'class': 'form-control  mt-2 mx-auto', 'placeholder': 'Vārds | Name | Имя',
 																			'onpaste': 'return false;',
 																			'ondrop': 'return false;',
 																			'autocomplete': 'off'}))
-    sender = forms.CharField(label='E-pasts', widget=forms.EmailInput(attrs={'class': 'form-control mt-2 mx-auto', 'placeholder': 'E-pasts/почта/email',
+    sender = forms.CharField(label='E-pasts', widget=forms.EmailInput(attrs={'class': 'form-control mt-2 mx-auto', 'placeholder': 'e-pasts | email | почта',
 																			'onpaste': 'return false;',
 																			'ondrop': 'return false;',
 																			'autocomplete': 'off'}))
-    subject = forms.CharField(label='Temats', widget=forms.TextInput(attrs={'class': 'form-control mt-2 mx-auto', 'placeholder': 'Temats/тема/topic',
+    subject = forms.CharField(label='Temats', widget=forms.TextInput(attrs={'class': 'form-control mt-2 mx-auto', 'placeholder': 'Temats | Topic | Тема',
 																			'onpaste': 'return false;',
 																			'ondrop': 'return false;',
 																			'autocomplete': 'off'}))
-    content = forms.CharField(label='Teksts', widget=forms.Textarea(attrs={'class': 'form-control mt-2 mx-auto', 'rows': 5, 'placeholder': 'Teksts/текст/text',
+    content = forms.CharField(label='Teksts', widget=forms.Textarea(attrs={'class': 'form-control mt-2 mx-auto', 'rows': 5, 'placeholder': 'Teksts | Text | Текст',
 																			'onpaste': 'return false;',
 																			'ondrop': 'return false;',
 																			'autocomplete': 'off'}))
-    # captcha = CaptchaField(label='Captcha', widget=CaptchaTextInput(attrs={'class': 'form-control d-block mx-auto', 'placeholder': 'Atbilde/ответ/answer'}))
+    # captcha = CaptchaField(label='Captcha', widget=CaptchaTextInput(attrs={'class': 'form-control d-block mx-auto', 'placeholder': 'atbilde | answer | ответ'}))
