28848b143346bf1f34d77a7a1747b00072e13fa0
LaSTiqq <lavrencij13@gmail.com>
2023-07-28 21:18:59 +0300

    Added Google Recaptcha

--- details/forms.py
+++ details/forms.py
@@ -1,14 +1,28 @@
 from django import forms
+from django.conf import settings
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV2Invisible
 from django.utils.translation import gettext_lazy as _
 
 
 class ContactForm(forms.Form):
     name = forms.CharField(label=_('Vārds'), widget=forms.TextInput(attrs={'class': 'form-control mt-2 mx-auto', 'placeholder': _('Jānis'),
-                                                                            'autocomplete': 'off', 'required': True}))
+                                                                           'autocomplete': 'off', 'required': True}))
     sender = forms.EmailField(label=_('E-pasts'), widget=forms.EmailInput(attrs={'class': 'form-control mt-2 mx-auto', 'placeholder': _('janis.celotajs@gmail.com'),
-                                                                            'autocomplete': 'off', 'required': True}))
+                                                                                 'autocomplete': 'off', 'required': True}))
     subject = forms.CharField(label=_('Temats'), widget=forms.TextInput(attrs={'class': 'form-control mt-2 mx-auto', 'placeholder': _('LED gaismas virtuvē'),
-                                                                            'autocomplete': 'off', 'required': True}))
-    content = forms.CharField(label=_('Teksts'), widget=forms.Textarea(attrs={'class': 'form-control mt-2 mx-auto', 'rows': 5, 
-                                                                            'placeholder': _('Vēlos izgaismot virtuvi, ko Jūs varat piedāvāt?'),
-                                                                            'autocomplete': 'off', 'required': True}))
+                                                                               'autocomplete': 'off', 'required': True}))
+    content = forms.CharField(label=_('Teksts'), widget=forms.Textarea(attrs={'class': 'form-control mt-2 mx-auto', 'rows': 5,
+                                                                              'placeholder': _('Vēlos izgaismot virtuvi, ko Jūs varat piedāvāt?'),
+                                                                              'autocomplete': 'off', 'required': True}))
+    captcha = ReCaptchaField(
+        label='Captcha',
+        widget=ReCaptchaV2Invisible(
+            attrs={
+                'data-sitekey': settings.RECAPTCHA_PUBLIC_KEY,
+            },
+            api_params={
+                'hl': 'en',
+            },
+        ),
+    )
