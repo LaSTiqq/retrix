7b118cf3bfc30190b1e24d91cec378f587efed4c
LaSTiqq <lavrencij13@gmail.com>
2023-07-29 19:59:46 +0300

    Little form styles simplification

--- details/forms.py
+++ details/forms.py
@@ -6,19 +6,20 @@ from django.utils.translation import gettext_lazy as _
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label=_('Vārds'), widget=forms.TextInput(attrs={'class': 'form-control mt-2 mx-auto', 'placeholder': _('Jānis'),
+    name = forms.CharField(label=_('Vārds'), widget=forms.TextInput(attrs={'class': 'form-control bg-transparent mt-2 mx-auto', 'placeholder': _('Jānis'),
                                                                            'autocomplete': 'off', 'required': True}))
-    sender = forms.EmailField(label=_('E-pasts'), widget=forms.EmailInput(attrs={'class': 'form-control mt-2 mx-auto', 'placeholder': _('janis.celotajs@gmail.com'),
+    sender = forms.EmailField(label=_('E-pasts'), widget=forms.EmailInput(attrs={'class': 'form-control bg-transparent mt-2 mx-auto', 'placeholder': _('janis.celotajs@gmail.com'),
                                                                                  'autocomplete': 'off', 'required': True}))
-    subject = forms.CharField(label=_('Temats'), widget=forms.TextInput(attrs={'class': 'form-control mt-2 mx-auto', 'placeholder': _('LED gaismas virtuvē'),
+    subject = forms.CharField(label=_('Temats'), widget=forms.TextInput(attrs={'class': 'form-control bg-transparent mt-2 mx-auto', 'placeholder': _('LED gaismas virtuvē'),
                                                                                'autocomplete': 'off', 'required': True}))
-    content = forms.CharField(label=_('Teksts'), widget=forms.Textarea(attrs={'class': 'form-control mt-2 mx-auto', 'rows': 5,
+    content = forms.CharField(label=_('Teksts'), widget=forms.Textarea(attrs={'class': 'form-control bg-transparent mt-2 mx-auto pt-3', 'rows': 6,
                                                                               'placeholder': _('Vēlos izgaismot virtuvi, ko Jūs varat piedāvāt?'),
                                                                               'autocomplete': 'off', 'required': True}))
     captcha = ReCaptchaField(
         label='Captcha',
         widget=ReCaptchaV2Invisible(
             attrs={
+                'style': 'position: relative; z-index: 3;',
                 'data-sitekey': settings.RECAPTCHA_PUBLIC_KEY,
             },
             api_params={
