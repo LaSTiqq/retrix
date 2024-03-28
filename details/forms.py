8bd9b656b377e3f799e5914eee4ff6a720304504
LaSTiqq <lavrencij13@gmail.com>
2024-03-28 22:20:24 +0200

    Some email HTML changes #2

--- details/forms.py
+++ details/forms.py
@@ -6,15 +6,15 @@ from django.utils.translation import gettext_lazy as _
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label=_('Vārds'), widget=forms.TextInput(attrs={'class': 'form-control bg-transparent mt-2 mx-auto', 'placeholder': _('Jānis'),
-                                                                           'autocomplete': 'off', 'required': True, 'maxlength': '15'}))
-    sender = forms.EmailField(label=_('E-pasts'), widget=forms.EmailInput(attrs={'class': 'form-control bg-transparent mt-2 mx-auto', 'placeholder': _('janis.celotajs@gmail.com'),
-                                                                                 'autocomplete': 'off', 'required': True}))
-    subject = forms.CharField(label=_('Temats'), widget=forms.TextInput(attrs={'class': 'form-control bg-transparent mt-2 mx-auto', 'placeholder': _('LED gaismas virtuvē'),
-                                                                               'autocomplete': 'off', 'required': True, 'maxlength': '30'}))
-    content = forms.CharField(label=_('Teksts'), widget=forms.Textarea(attrs={'class': 'form-control bg-transparent mt-2 mx-auto pt-3', 'rows': 6,
-                                                                              'placeholder': _('Vēlos izgaismot virtuvi, ko Jūs varat piedāvāt?'),
-                                                                              'autocomplete': 'off', 'required': True}), min_length=50)
+    name = forms.CharField(label=_('Vārds'), widget=forms.TextInput(attrs={
+        'class': 'form-control bg-transparent mt-2 mx-auto', 'placeholder': _('Jānis'), 'autocomplete': 'off', 'maxlength': '15'}))
+    sender = forms.EmailField(label=_('E-pasts'), widget=forms.EmailInput(attrs={
+        'class': 'form-control bg-transparent mt-2 mx-auto', 'placeholder': _('janis.celotajs@gmail.com'), 'autocomplete': 'off', }))
+    subject = forms.CharField(label=_('Temats'), widget=forms.TextInput(attrs={
+        'class': 'form-control bg-transparent mt-2 mx-auto', 'placeholder': _('LED gaismas virtuvē'), 'autocomplete': 'off', 'maxlength': '30'}))
+    content = forms.CharField(label=_('Teksts'), widget=forms.Textarea(attrs={
+        'class': 'form-control bg-transparent mt-2 mx-auto pt-3', 'placeholder': _('Vēlos izgaismot virtuvi, ko Jūs varat piedāvāt?'), 'autocomplete': 'off', 'rows': 6}),
+        min_length=50)
     captcha = ReCaptchaField(
         label='Captcha',
         widget=ReCaptchaV3(
