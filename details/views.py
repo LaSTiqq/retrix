69f8ac4f922024a8618c12b6aec4faa1a80dec7f
LaSTiqq <lavrencij13@gmail.com>
2024-11-01 00:08:58 +0200

    Simplification of send email logic

--- details/views.py
+++ details/views.py
@@ -31,22 +31,15 @@ def send(request):
     if request.method == 'POST':
         form = ContactForm(data=request.POST)
         if form.is_valid():
-            if restricted_found(form.cleaned_data['name']):
-                messages.warning(
-                    request, _("Jūs ievadījāt kaut ko neatļautu! Mēģiniet vēlreiz."))
-                return redirect(_('/#communication'))
-            elif restricted_found(form.cleaned_data['subject']):
-                messages.warning(
-                    request, _("Jūs ievadījāt kaut ko neatļautu! Mēģiniet vēlreiz."))
-                return redirect(_('/#communication'))
-            elif restricted_found(form.cleaned_data['content']):
+            if any(restricted_found(form.cleaned_data[field]) for field in ['name', 'subject', 'content']):
                 messages.warning(
                     request, _("Jūs ievadījāt kaut ko neatļautu! Mēģiniet vēlreiz."))
                 return redirect(_('/#communication'))
             html_content = render_to_string('email.html', {
-                                            'name': form.cleaned_data['name'],
-                                            'sender': form.cleaned_data['sender'],
-                                            'content': form.cleaned_data['content']})
+                'name': form.cleaned_data['name'],
+                'sender': form.cleaned_data['sender'],
+                'content': form.cleaned_data['content']
+            })
             text_content = strip_tags(html_content)
             try:
                 email = EmailMultiAlternatives(
